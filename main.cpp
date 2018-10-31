beb4741cb5a8f4def831fceaf8b7431efa9923f0
ndang008 <ndang008@ucr.edu>
2018-10-31 16:40:22 +0000

    Priority Queue working with State*, display puzzle working

--- main.cpp
+++ main.cpp
@@ -4,59 +4,73 @@
 
 using namespace std; 
 
-class Node
+class State
 {
     public:   //for sake of project keep public
-        Node(int puzzle [9], int cost)
+        State(int puzzle [9], int hn, int gn, int fn, State* parent) //constructor
         {
             for (int i = 0; i < 9; i++)
             {
                 this->puzzle[i] = puzzle[i];
             }
-            fn = cost; 
+            this->hn = hn;
+            this->gn = gn;
+            this->fn = fn;
+            this->parent = parent;
         }
-        int puzzle [];
+        
+        void displayPuzzle()
+        {
+            cout << "g(n) = " << gn << " " << "h(n) = " << fn << endl;
+            cout << puzzle[0] << "  " << puzzle[1] << "  " << puzzle[2] << endl;
+            cout << puzzle[3] << "  " << puzzle[4] << "  " << puzzle[5] << endl;
+            cout << puzzle[6] << "  " << puzzle[7] << "  " << puzzle[8] << endl;
+        }
+        
+        
+        
+        //variables
+        State* parent; 
+        int puzzle [9];
+        int hn;
+        int gn;
         int fn;
 };
 
-class compareNode
+class compareState
 {
     public: 
-        int operator() (const Node& node1, const Node& node2)
+        int operator() (State* state1, State* state2)
         {
-            return node1.fn > node2.fn;
+            return state1->fn > state2->fn;
         }
 };
 
+//void general
+
 int main()
 {
-    int puzzle [9] = {1,2,3,4,5,6,7,8,0};
+    int userPuzzle[9];
+    cout << "Enter 3 tiles in first row following each tile with a return: ";
+    cin >> userPuzzle[0];
+    cin >> userPuzzle[1];
+    cin >> userPuzzle[2];
+    cout << "Enter 3 tiles in first row following each tile with a return: ";
+    cin >> userPuzzle[3];
+    cin >> userPuzzle[4];
+    cin >> userPuzzle[5];
+    cout << "Enter 3 tiles in first row following each tile with a return: ";
+    cin >> userPuzzle[6];
+    cin >> userPuzzle[7];
+    cin >> userPuzzle[8];
     
-    Node node1(puzzle, 1);
-    Node node2(puzzle, 3);
-    Node node3(puzzle, 6);
-    Node node4(puzzle, 10);
+    State* initState = new State(userPuzzle, 0, 0, 0, 0);
     
-    priority_queue <Node, vector<Node>, compareNode > pq; 
     
-    pq.push(node1);
-    pq.push(node2);
-    pq.push(node3);
-    pq.push(node4);
+    priority_queue <State*, vector<State*>, compareState> pq; 
+    pq.push(initState);
     
-    cout << pq.top().fn << endl;
-    pq.pop();
-    cout << pq.top().fn << endl;
-    pq.pop();
-    
-    pq.push(node1);
-    
-    cout << pq.top().fn << endl;
-    pq.pop();
-    cout << pq.top().fn << endl;
-    pq.pop();
-    cout << pq.top().fn << endl;
-    pq.pop();
+    pq.top()->displayPuzzle();
     
     return 0; 
-}
+} 
