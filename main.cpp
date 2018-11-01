b96ba0e8b86564f565e3b156f26386e9521beb1b
ndang008 <ndang008@ucr.edu>
2018-11-01 06:25:06 +0000

    Need to implement move functions and a function to check what moves are possible

--- main.cpp
+++ main.cpp
@@ -7,12 +7,13 @@ using namespace std;
 class State
 {
     public:   //for sake of project keep public
-        State(int puzzle [9], int hn, int gn, int fn, State* parent) //constructor
+        State(int puzzle [9], int blank_index, int hn, int gn, int fn, State* parent) //constructor
         {
             for (int i = 0; i < 9; i++)
             {
                 this->puzzle[i] = puzzle[i];
             }
+            this->blank_index = blank_index;
             this->hn = hn;
             this->gn = gn;
             this->fn = fn;
@@ -27,11 +28,30 @@ class State
             cout << puzzle[6] << "  " << puzzle[7] << "  " << puzzle[8] << endl;
         }
         
+        State* moveUp()
+        {
+            return 0; //fixme
+        }
         
+        State* moveDown()
+        {
+            return 0; //fixme
+        }
+        
+        State* moveLeft()
+        {
+            return 0; //fixme  
+        }
+        
+        State* moveRight()
+        {
+            return 0; //fixme
+        }
         
         //variables
         State* parent; 
         int puzzle[9];
+        int blank_index;
         int hn;
         int gn;
         int fn;
@@ -46,25 +66,74 @@ class compareState //compare function for priority queue
         }
 };
 
-void general_search(State* initialState, int algChoice)
+void general_search(State* problem, int algChoice)
 {
     int heuristic = algChoice;
-    int goal_state[9] = {1,2,3,4,5,6,7,8,0};
+    int goal_state[9] = {1,2,3,4,5,6,8,7,0};
+    int states_expanded = 0;
+    int max_queue_size = 0;
     
-    priority_queue <State*, vector<State*>, compareState> pq; 
+    priority_queue <State*, vector<State*>, compareState > pq; 
     
-    pq.push(initialState);
-    pq.top()->displayPuzzle();
+    State* currNode; 
+    
+    pq.push(problem);
+    
+    while(pq.size() != 0)
+    {
+        if (pq.size() > max_queue_size) //update max queue size for statistics
+        {
+            max_queue_size = pq.size();
+        }
+        
+        cout << "checking state";
+        currNode = pq.top();
+        pq.pop();
+        
+        for (int i = 0; i < 9; i++) //check for goal state
+        {
+            if (currNode->puzzle[i] != goal_state[i])
+            {
+                break;
+            }
+            else if ((currNode->puzzle[i] == goal_state[i]) && (i == 8))
+            {
+                currNode->displayPuzzle(); 
+                cout << "Puzzle Solved!" << endl;
+                return;
+            }
+        }
+        
+        
+        if (algChoice == 1) //uniform cost
+        {
+            cout << "FIXME" << endl;
+        }
+        else if (algChoice == 2) //misplaced tile
+        {
+            cout << "FIXME" << endl;
+        }
+        else if (algChoice == 3) //manhattan
+        {
+            cout << "FIXME" << endl; 
+        }
+    }
+    
+    cout << "No solution found!" << endl;
 }
+
+
 
 //void general
 
 int main()
 {
-    int userPuzzle[9];
-    int algChoice; 
+    //int userPuzzle[9];
+    int userPuzzle[9] = {1,2,3,4,5,6,7,8,0}; //TESTING
+    int algChoice;
+    int blank_index; 
     
-    cout << "Enter 3 tiles in first row following each tile with a return: ";
+   /* cout << "Enter 3 tiles in first row following each tile with a return: ";
     cin >> userPuzzle[0];
     cin >> userPuzzle[1];
     cin >> userPuzzle[2];
@@ -75,7 +144,17 @@ int main()
     cout << "Enter 3 tiles in first row following each tile with a return: ";
     cin >> userPuzzle[6];
     cin >> userPuzzle[7];
-    cin >> userPuzzle[8];
+    cin >> userPuzzle[8]; */
+    
+    for (int i = 0; i < 9; i++)
+    {
+        if (userPuzzle[i] == 0)
+        {
+            blank_index = i;
+        }
+    }
+    
+    
     
     cout << "Enter algorithm choice: " << endl;
     cout << "   Enter \"1\" for Uniform Cost Search" << endl;
@@ -84,9 +163,9 @@ int main()
     
     cin >> algChoice;
     
-    State* initState = new State(userPuzzle, 0, 0, 0, 0);
+    State* problem = new State(userPuzzle, 0, 0, 0, 0, 0);
     
-    general_search(initState, algChoice);
+    general_search(problem, algChoice);
     
     
     return 0; 
